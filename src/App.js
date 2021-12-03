308d24ab29404d4e0187cb290fb4d0c7ee260c04
Horvath, Balazs (IIT/GSD/Technical Systems) <balazs.horvath@aldi-sued.com>
2021-12-03 14:48:50 +0100

    cpu core table started

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import TempGauge from "./components/TempGauge";
 import LoadGauge from "./components/LoadGauge";
 import DateTime from "./components/DateTime";
 import Weather from "./components/Weather";
+
+
 import "./style/app.css"
 
 import config from './config';
 import { useEffect, useState } from 'react';
+
+
+import { BsCpu } from "react-icons/bs";
+import { BsServer } from "react-icons/bs";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { ImPower } from "react-icons/im";
+
 
 
 function App() {
@@ -55,21 +64,52 @@ function App() {
       </div>
 
 
-      <div id="gagues" className="grid-container">
-        <div className="grid-item">
+      <div id="gauges" className="container">
+        <div className="flex-col">
           <h3>{cpuData.name}</h3>
-          <div className="grid-cont-2">
-            <div className="grid-item">
+          <div className="flex-row">
+            <div className="flex-col">
               <span className="gauge-heading" >Temperature</span>
               <TempGauge temperature={cpuData.total.temp} />
             </div>
-            <div className="grid-item">
+            <div className="flex-col">
               <span className="gauge-heading">Load</span>
               <LoadGauge load={cpuData.total.load} />
             </div>
           </div>
-          <div className="grid-container">
-            <div className="grid-item"></div>
+          <div style={{width: "100%"}} className="flex-col">
+            <table>
+              <thead>
+                <th><BsCpu /></th>
+                <th><BsServer /></th>
+                <th><AiOutlineClockCircle /></th>
+                <th><ImPower /></th>
+              </thead>
+              {cpuData.cores.map(core =>{
+                return (
+                  <tr>
+                    <td>{core.name}</td>
+                    <td>{core.load} %</td>
+                    <td>{core.clock} Hz</td>
+                    <td>{core.power} V</td>
+                  </tr>
+                )
+              })}
+            </table>
+          </div>
+
+        </div>
+        <div className="flex-col">
+          <h3>GIGABYTE GTX 1660 SUPER</h3>
+          <div className="flex-row">
+            <div className="flex-col">
+              <span className="gauge-heading" >Temperature</span>
+              <TempGauge temperature={cpuData.total.temp} />
+            </div>
+            <div className="flex-col">
+              <span className="gauge-heading">Load</span>
+              <LoadGauge load={cpuData.total.load} />
+            </div>
           </div>
         </div>
       </div>
